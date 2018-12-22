9ff5bc51c79c8d0939f935a69940a38bc6f05ce9
Kevin Choy <freecancerian@gmail.com>
2018-12-22 15:42:52 +0000

    added friendship functions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+  get "search_friends", to: "users#search"
   get "my_friends", to: "users#my_friends"
+  post "add_friend", to: "users#add_friend"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
